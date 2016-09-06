d8eb94637ed4fffde9139ed80045380320dabb93
Georgina Van Dort and Karen Freeman-Smith <karen@>
2016-09-06 13:19:22 -0700

    add user front end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,12 @@
+import java.io.Console;
+import java.util.List;
+
 public class App{
   public static void main (String[] args) {
-
-    System.out.println("Hello ping-pong!");
-    
+    Console c = System.console();
+    int userNumber = Integer.parseInt(c.readLine("Enter a number "));
+    PingPong pingPong = new PingPong();
+    List<Object> pingPongResult = pingPong.runPingPong(userNumber);
+    System.out.println(pingPongResult);
   }
 }
